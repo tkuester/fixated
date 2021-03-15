656668e8a82fce2d9b7ac58994bfc3331df9223d
Tim K <tkuester@users.noreply.github.com>
2021-03-15 09:16:33 -0400

    Add scm version to init

--- fixated/__init__.py
+++ fixated/__init__.py
@@ -1,2 +1,11 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
 from . import nmea
-from . serial_parser import SerialNmeaParser
+from .datatypes import TPV
+from .serial_parser import SerialNmeaParser
+from .gpsd_sock import GpsdSocket
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = "unknown"
